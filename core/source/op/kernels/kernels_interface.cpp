117a970d25796b150e5fc3eea7e5dabf2f660df8
CloudTvT <163865120+CloudTvT@users.noreply.github.com>
2025-07-08 17:01:05 +0800

    Add files via upload

--- core/source/op/kernels/kernels_interface.cpp
+++ core/source/op/kernels/kernels_interface.cpp
@@ -1,5 +1,15 @@
 #include <base/base.h>
 #include "cpu/add_kernel.h"
+#include "cpu/emb_kernel.h"
+#include "cpu/matmul_kernel.h"
+#include "cpu/mha_kernel.h"
+#include "cpu/rmsnorm_kernel.h"
+#include "cpu/rope_kernel.h"
+#include "cpu/softmax_kernel.h"
+#include "cpu/swiglu_kernel.h"
+#include "cpu/scale_kernel.h"
+#include "cpu/scale_sum_kernel.h"
+
 #include "cuda/add_kernel.cuh"
 #include "kernels_interface.h"
 
@@ -14,4 +24,104 @@ AddKernel get_add_kernel(kuiper_base::DeviceType device_type) {
     return nullptr;
   }
 }
+
+EmbeddingKernel get_emb_kernel(kuiper_base::DeviceType device_type) {
+  if (device_type == kuiper_base::DeviceType::kDeviceCPU) {
+    return emb_kernel_normal;
+  } else if (device_type == kuiper_base::DeviceType::kDeviceCUDA) {
+    LOG(FATAL) << "Cuda device is not implemented.";
+    return nullptr;
+  } else {
+    LOG(FATAL) << "Unknown device type for get a add kernel.";
+    return nullptr;
+  }
+}
+
+MatmulKernel get_matmul_kernel(kuiper_base::DeviceType device_type) {
+  if (device_type == kuiper_base::DeviceType::kDeviceCPU) {
+    return matmul_kernel_cpu;
+  } else if (device_type == kuiper_base::DeviceType::kDeviceCUDA) {
+    LOG(FATAL) << "Cuda device is not implemented.";
+    return nullptr;
+  } else {
+    LOG(FATAL) << "Unknown device type for get a add kernel.";
+    return nullptr;
+  }
+}
+
+RMSNormKernel get_rmsnorm_kernel(kuiper_base::DeviceType device_type) {
+  if (device_type == kuiper_base::DeviceType::kDeviceCPU) {
+    return rmsnorm_kernel_cpu;
+  } else if (device_type == kuiper_base::DeviceType::kDeviceCUDA) {
+    LOG(FATAL) << "Cuda device is not implemented.";
+    return nullptr;
+  } else {
+    LOG(FATAL) << "Unknown device type for get a add kernel.";
+    return nullptr;
+  }
+}
+
+SwigluKernel get_swiglu_kernel(kuiper_base::DeviceType device_type, void* stream) {
+  if (device_type == kuiper_base::DeviceType::kDeviceCPU) {
+    return swiglu_kernel_cpu;
+  } else if (device_type == kuiper_base::DeviceType::kDeviceCUDA) {
+    LOG(FATAL) << "Cuda device is not implemented.";
+    return nullptr;
+  } else {
+    LOG(FATAL) << "Unknown device type for get a swiglu kernel.";
+    return nullptr;
+  }
+}
+
+SoftmaxInplaceKernel get_softmax_kernel(kuiper_base::DeviceType device_type) {
+  if (device_type == kuiper_base::DeviceType::kDeviceCPU) {
+    return softmax_inplace_cpu;
+  } else {
+    LOG(FATAL) << "Unknown device type for get an softmax kernel.";
+    return nullptr;
+  }
+}
+
+MHAKernel get_mha_kernel(kuiper_base::DeviceType device_type) {
+  if (device_type == kuiper_base::DeviceType::kDeviceCPU) {
+    return mha_kernel;
+  } else if (device_type == kuiper_base::DeviceType::kDeviceCUDA) {
+    LOG(FATAL) << "Cuda device is not implemented.";
+    return nullptr;
+  } else {
+    LOG(FATAL) << "Unknown device type for get an mha kernel.";
+    return nullptr;
+  }
+}
+
+RoPEKernel get_rope_kernel(kuiper_base::DeviceType device_type) {
+  if (device_type == kuiper_base::DeviceType::kDeviceCPU) {
+    return rope_kernel_cpu;
+  } else if (device_type == kuiper_base::DeviceType::kDeviceCUDA) {
+    LOG(FATAL) << "Cuda device is not implemented.";
+    return nullptr;
+  } else {
+    LOG(FATAL) << "Unknown device type for get a rope kernel.";
+    return nullptr;
+  }
+}
+
+ScaleKernel get_scale_kernel(kuiper_base::DeviceType device_type) {
+  if (device_type == kuiper_base::DeviceType::kDeviceCPU) {
+    return scale_inplace_cpu;
+  } else {
+    LOG(FATAL) << "Unknown device type for get a rope kernel.";
+    return nullptr;
+  }
+}
+
+ScaleSumKernel get_scale_sum_kernel(kuiper_base::DeviceType device_type) {
+  if (device_type == kuiper_base::DeviceType::kDeviceCPU) {
+    return scale_sum_kernel_cpu;
+  } else {
+    LOG(FATAL) << "Unknown device type for get a scale and reduce kernel.";
+    return nullptr;
+  }
+}
+
 }
